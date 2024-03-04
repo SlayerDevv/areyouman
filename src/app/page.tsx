b1eb5fe8937433a21929dddabacf38d95da6d382
SlayerDevv <hdfhoussam18@gmail.com>
2024-03-04 19:57:27 +0100

    UPDATES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
 
   const handlemove = () => { 
     const button = document.getElementById('nobtn') as HTMLButtonElement
-    const x = Math.floor(Math.random() * button.offsetLeft + button.offsetWidth)
-    const y = Math.floor(Math.random() * button.offsetTop  + button.offsetHeight)
+    const x = Math.floor(Math.random() * 100)
+    const y = Math.floor(Math.random() * 100)
     
     button.style.left = `${x}px`
     button.style.top = `${y}px`
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="flex space-x-5">
       <span className="text-xl " ><h1 id="txt">Are You Gay ?</h1></span>
       <button className="bg-blue-600 w-[60px] h-[60px] rounded-xl" id="yesbtn" onClick={() => yes()}>Yes</button>
-      <button className="absolute bg-blue-600  rounded-xl right-[500px] w-[60px] h-[60px] " id="nobtn" onClick={() => handlemove()}>No</button>
+      <button className="absolute fixed bg-blue-600  rounded-xl md:right-[500px] md:left-[706px] left-[210px] top-[200px] w-[60px] h-[60px] " id="nobtn" onClick={() => handlemove()}>No</button>
       </div>
     </div>
   );
